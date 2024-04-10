4ee203317ebd7bf289179057935317e1376d3d06
kiptoobrachok <calebkiptoo9090@gmail.com>
2024-04-10 14:05:07 +0300

    dictionaries

--- pythonDictionaries/0-main.py
+++ pythonDictionaries/0-main.py
@@ -5,6 +5,7 @@
 import sys
 
 from robotDict import myRobotDict
+from dict_constructor import secondRobotDict
 
 if __name__ == '__main__':
     print(f"Name : {myRobotDict['name']}")
@@ -23,3 +24,30 @@ if __name__ == '__main__':
     #check number of items in dictionary
     print(f"Number of items in my dict : {len(myRobotDict)}")
 
+
+    "Using the dict() constructor"
+    print(secondRobotDict)
+
+    print(f"second robot Name : {secondRobotDict['name']}")
+
+
+    # Accessing dictionary items
+    first_robot = myRobotDict['name']
+    print(f"my first robot : {first_robot}")
+
+    #using get() method
+    second_robot = secondRobotDict.get("name")
+    print(f"My second robot: {second_robot}")
+
+    #keys() - this returns all keys in the dictionary
+    print(myRobotDict.keys())
+    print(myRobotDict.values)
+
+    # values() - this returns all values
+    print(secondRobotDict.keys())
+    print(secondRobotDict.values())
+
+
+    #items() returns the items of the dictionary as tuples in a list
+    print(secondRobotDict.items())
+    
---
+++ pythonDictionaries/dict_constructor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+It is also possible to use dict() to make a dictionary
+"""
+
+secondRobotDict = dict(name = "AURA-RGDX", model = "AURA", year = 2023, Type = "AI")
+
