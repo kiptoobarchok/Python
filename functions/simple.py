7c1e40fe9383e185969b84a8bb58778f3c19d2ce
kiptoobarchok <calebkiptoo9090@gmail.com>
2024-06-17 13:40:33 +0300

    python

---
+++ functions/simple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+a simple definition of a function
+'''
+
+# def greet():
+#     name = input("Enter your name: ")
+#     print(f"Hi {name}")
+
+
+def hello(name):
+    print(f"Hello {name}")
+
+hello("Caleb")
---
+++ lists/lists.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'nested list'
+
+siblings = [['caleb', 21], ['abel', 26], ['peter', 17], ['kevin', 21]]
+
+print(siblings[0][0])
+
+for i in siblings:
+    print(i)
